7d27a6246ef32f55cb31bdefed9644365fec2655
Nurbu <lamanurbu2@gmail.com>
2026-05-11 13:23:53 -0400

    Added the private fields, getters and setters to Vehicle class.

--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
-
 public class Vehicle {
+
+    private String color;
+    private int numberOfPassengers;
+    private int cargoCapacity;
+    private int fuelCapacity;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getNumberOfPassengers() {
+        return numberOfPassengers;
+    }
+
+    public void setNumberOfPassengers(int numberOfPassengers) {
+        this.numberOfPassengers = numberOfPassengers;
+    }
+
+    public int getCargoCapacity() {
+        return cargoCapacity;
+    }
+
+    public void setCargoCapacity(int cargoCapacity) {
+        this.cargoCapacity = cargoCapacity;
+    }
+
+    public int getFuelCapacity() {
+        return fuelCapacity;
+    }
+
+    public void setFuelCapacity(int fuelCapacity) {
+        this.fuelCapacity = fuelCapacity;
+    }
 }
